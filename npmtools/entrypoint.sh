99623d84322c22862bda8bc3cba58b5eca2777b3
Grégoire Paris <postmaster@greg0ire.fr>
2015-08-16 14:51:19 +0200

    shellcheck

--- npmtools/entrypoint.sh
+++ npmtools/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-if [ $USERNAME ]; then
+if [ "$USERNAME" ]; then
 
-    if [ -z $UNIX_UID ]; then
+    if [ -z "$UNIX_UID" ]; then
         UNIX_UID=1001
     fi
 
     # Create user
-    useradd --uid $UNIX_UID --home /home/developer $USERNAME
+    useradd --uid "$UNIX_UID" --home /home/developer "$USERNAME"
 
-    chown $USERNAME:$USERNAME /home/developer
+    chown "$USERNAME:$USERNAME" /home/developer
 
 fi
 
